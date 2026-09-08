f18800b4247c0cea838e23b48c2c421c26110c64
mac <xjh132432@gmail.com>
2026-09-08 21:47:23 +0800

    day01: learn C++ multi-file compilation and linking

---
+++ day01_cpp_project/calculator.cpp
@@ -0,0 +1,12 @@
+#include "calculator.h"
+
+int add(int a,int b)
+{
+    return a+b;
+
+}
+
+int sub(int a,int b)
+{
+    return a-b;
+}
---
+++ day01_cpp_project/main.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+#include "calculator.h"
+
+int main()
+{
+    std::cout<< add(3,5)<<std::endl;
+    std::cout << sub(5, 3) << '\n';
+
+    return 0;
+}
