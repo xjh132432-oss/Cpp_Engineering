1a6a926382cf20a8f795f71bac4fc5840f2bfa4c
mac <xjh132432@gmail.com>
2026-09-09 23:27:52 +0800

    day02 learn guards and modular

---
+++ day02/calculator.cpp
@@ -0,0 +1,22 @@
+#include "calculator.h"
+
+int add(int a,int b)
+{
+    return a+b;
+
+}
+
+int sub(int a,int b)
+{
+    return a-b;
+}
+
+int multiply(int a, int b)
+{
+    return a * b;
+}
+
+int divide(int a, int b)
+{
+    return a / b;
+}
---
+++ day02/test.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "calculator.h"
+
+int main()
+{
+    std::cout << "add: " << add(3, 5) << '\n';
+    
+    return 0;
+}
