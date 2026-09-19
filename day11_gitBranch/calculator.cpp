50ef4e35149e76af001fdd2b060c97d9fe90a344
mac <xjh132432@gmail.com>
2026-09-19 22:41:53 +0800

    day11 branch text: add multiply function

---
+++ day11_gitBranch/calculator.cpp
@@ -0,0 +1,17 @@
+#include "calculator.h"
+
+int add(int a, int b)
+{
+    return a + b;
+}
+
+int sub(int a, int b)
+{
+    return a - b;
+}
+
+int multiply(int a,int b)
+{
+	return a*b;
+}
+	
---
+++ day11_gitBranch/main.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+#include "calculator.h"
+
+int main()
+{
+	std::cout<<"add:"<<add(10,5)<<std::endl;
+	std::cout<<"sub:"<<sub(10,5)<<std::endl;
+	std::cout<<"multiply:"<<multiply(10,5)<<std::endl;
+	return 0;
+}
